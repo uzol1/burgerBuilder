4780a464078ed4e15592577c93d40eb9b802d0a6
uzol1 <uzol12345@gmail.com>
2021-04-08 15:08:42 +0545

    deleted

--- src/component/UI/Button/Button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Classes from './Button.css'
-const button = (props) => (
-    <button onClick={props.clicked}
-        className={[Classes.Button, Classes[props.btnType]].join(' ')}
-    >{props.children}</button>
-)
-export default button;
