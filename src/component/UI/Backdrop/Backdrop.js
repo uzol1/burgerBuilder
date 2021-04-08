4c1f6f0d45b88edb1ba4273e8979e1b0d49e3e34
uzol1 <uzol12345@gmail.com>
2021-04-08 16:27:21 +0545

    deleted

--- src/component/UI/Backdrop/Backdrop.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import Classes from './Backdrop.css'
-
-const backdrop = (props) => (
-    props.show ? <div className={Classes.Backdrop} onClick={props.orderCancel}></div> : null
-)
-export default backdrop
